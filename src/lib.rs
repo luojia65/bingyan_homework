3f633a4f42d6f3ad4ad6b06103e9fbc6789ee657
luojia65 <me@luojia.cc>
2018-09-30 19:40:03 +0800

    Basic road map

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,18 @@
 
 
+/*
+1.  select .., .., .. from table .. where .. = .. limit ...
+2.  insert into ... value(...)
+3.
+*/
+
+// A simple B+ Tree
+struct DogeTree {
+
+}
+
+#[cfg(test)]
+mod dogetree_tests {
+
+}
+
